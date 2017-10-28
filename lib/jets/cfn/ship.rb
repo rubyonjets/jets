271536aa1b3ffeb3c292cf5b8f307f8cdfcf6ad4
Tung Nguyen <tongueroo@gmail.com>
2017-10-28 13:43:52 -0700

    time how long the code upload to s3 takes

--- lib/jets/cfn/ship.rb
+++ lib/jets/cfn/ship.rb
@@ -136,10 +136,22 @@ class Jets::Cfn
       end
 
       puts "Uploading #{zip_path} (#{file_size}) to S3"
+      start_time = Time.now
       key = Jets::Naming.code_s3_key
       obj = s3_resource.bucket(bucket_name).object(key)
       obj.upload_file(zip_path)
+      puts "Took #{pretty_time(Time.now-start_time)} to upload code to s3."
     end
 
+    # http://stackoverflow.com/questions/4175733/convert-duration-to-hoursminutesseconds-or-similar-in-rails-3-or-ruby
+    def pretty_time(total_seconds)
+      minutes = (total_seconds / 60) % 60
+      seconds = total_seconds % 60
+      if total_seconds < 60
+        "#{seconds.to_i}s"
+      else
+        "#{minutes.to_i}m #{seconds.to_i}s"
+      end
+    end
   end
 end
