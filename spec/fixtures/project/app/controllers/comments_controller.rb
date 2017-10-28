b4be6f3b30d13d5f54d1e3c503f12518b2cc0a7d
Tung Nguyen <tongueroo@gmail.com>
2017-10-28 09:25:32 -0700

    update jets

--- spec/fixtures/project/app/controllers/comments_controller.rb
+++ spec/fixtures/project/app/controllers/comments_controller.rb
@@ -1,7 +1,7 @@
 class CommentsController < Jets::BaseController
   def index
     puts "event #{event.inspect}"
-    render json: event.merge(a: "index3"), status: 200
+    render json: event.merge(a: "index4"), status: 200
   end
 
   def edit
