b8ee8e1679dbda46849dc1d0a6a34c90114d59a3
Tung Nguyen <tongueroo@gmail.com>
2017-10-30 10:28:28 -0700

    fix edge cases when minimal stack fails

--- lib/jets/delete.rb
+++ lib/jets/delete.rb
@@ -13,8 +13,7 @@ class Jets::Delete
     confirm_project_exists
 
     # Must first remove all objects from s3 bucket in order to delete stack
-    puts "First, deleting objects in s3 bucket #{s3_bucket_name}"
-    empty_bucket(s3_bucket_name)
+    empty_s3_bucket
 
     cfn.delete_stack(stack_name: parent_stack_name)
     puts "Project #{Jets::Config.project_namespace} deleted!"
@@ -31,18 +30,10 @@ class Jets::Delete
     end
   end
 
-  def s3_bucket_name
-    return @s3_bucket_name if @s3_bucket_name
-    resp = cfn.describe_stacks(stack_name: parent_stack_name)
-    output = resp.stacks[0].outputs.find {|o| o.output_key == 'S3Bucket'}
-    @s3_bucket_name = output.output_value
-  end
+  def empty_s3_bucket
+    return unless s3_bucket_name # Happens when minimal stack fails to build
 
-  def parent_stack_name
-    Jets::Naming.parent_stack_name
-  end
-
-  def empty_bucket(bucket_name)
+    puts "First, deleting objects in s3 bucket #{s3_bucket_name}"
     resp = s3.list_objects(bucket: bucket_name)
     if resp.contents.size > 0
       # IE: objects = [{key: "objectkey1"}, {key: "objectkey2"}]
@@ -58,11 +49,27 @@ class Jets::Delete
     end
   end
 
+  def s3_bucket_name
+    return @s3_bucket_name if defined?(@s3_bucket_name) != "nil"
+
+    resp = cfn.describe_stacks(stack_name: parent_stack_name)
+    outputs = resp.stacks[0].outputs
+    if outputs.empty?
+      @s3_bucket_name = false
+    else
+      @s3_bucket_name = outputs.find {|o| o.output_key == 'S3Bucket'}.output_value
+    end
+  end
+
+  def parent_stack_name
+    Jets::Naming.parent_stack_name
+  end
+
   def are_you_sure?
     if @options[:sure]
       sure = 'y'
     else
-      puts "Are you sure you want to want to delete the '#{project_with_env}' project? (y/N)"
+      puts "Are you sure you want to want to delete the '#{Jets::Config.project_namespace}' project? (y/N)"
       sure = $stdin.gets
     end
 
--- lib/jets/deploy.rb
+++ lib/jets/deploy.rb
@@ -25,12 +25,13 @@ class Jets::Deploy
 
 private
   def get_stack_options(first_run)
+    puts "parent_stack_name #{parent_stack_name}".colorize(:red)
     if first_run
       puts "FIRST RUN"
       {stack_type: "minimal"}
     else
       puts "SECOND RUN"
-      resp = cfn.describe_stacks(stack_name: parent_stack_name)
+      resp = check_updatable_status # exit if stack status is not in an updated able state
       output = resp.stacks[0].outputs.find {|o| o.output_key == 'S3Bucket'}
       s3_bucket = output.output_value
       {stack_type: "full", s3_bucket: s3_bucket}
@@ -44,5 +45,27 @@ private
 
   def parent_stack_name
     Jets::Naming.parent_stack_name
+  end
+
+  # All CloudFormation states listed here: http://docs.aws.amazon.com/AWSCloudFormation/latest/UserGuide/using-cfn-describing-stacks.html
+  #
+  # Returns resp so we can use it to grab data about the stack without calling api again.
+  def check_updatable_status
+    # Assumes stack exists
+    resp = cfn.describe_stacks(stack_name: parent_stack_name)
+    status = resp.stacks[0].stack_status
+    if status =~ /_IN_PROGRESS$/
+      puts "The '#{parent_stack_name}' stack status is #{status}." \
+           "It is not in an updateable status. Please check the stack and try again."
+      exit 0
+    elsif resp.stacks[0].outputs.empty?
+      # This Happens when the miminal stack fails at the very beginning.
+      # There is no s3 bucket at all.  User should delete the stack.
+      puts "The minimal stack failed to create. Please delete the stack first and try again." \
+      "You can delete the CloudFormation stack or use the `jets delete` command"
+      exit 0
+    else
+      resp
+    end
   end
 end
