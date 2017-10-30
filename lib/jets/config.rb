f661b69aaf7b064498bc44a1f20b976eb7c26fba
Tung Nguyen <tongueroo@gmail.com>
2017-10-30 08:34:51 -0700

    add support/console

--- lib/jets/config.rb
+++ lib/jets/config.rb
@@ -5,7 +5,7 @@ require 'yaml'
 # Overriden with config/project.yml
 class Jets::Config
   class << self
-    # Allows calling top level settings with the dot notation
+    # Allows calling settings with the dot notation:
     # Jets::Config.project_name
     # Jets::Config.timeout
     # Jets::Config.level1_option.level2_option
@@ -17,9 +17,13 @@ class Jets::Config
 
   # The settings from the files get merged with the following precedence:
   #
-  # current folder - The current folder’s config/application.yml values take the highest precedence.
+  # current folder - The projects config/application.yml values take the highest precedence.
   # user - The user’s ~/.jets/application.yml values take the second highest precedence.
   # default - The default settings bundled with the jets tool takes the lowest precedence.
+  #
+  # Even though default env can be configured in the config/settings.yml file.
+  # The JETS_ENV environment variable can be set and takes highest precedence
+  # over the settings file.
   #
   # More info: http://rubyonjets.com/docs/settings/
   @@settings = nil
@@ -35,10 +39,10 @@ class Jets::Config
     defaults_file = File.expand_path("../default/application.yml", __FILE__)
     defaults = YAML.load_file(defaults_file)
 
+    # Merge it all together
     settings = defaults.deep_merge(user.deep_merge(project))
-    # Even though default env can be configured in the config/settings.yml file.
-    # The JETS_ENV environment variable can be set and takes higher precedence
-    # over the settings file.
+
+    # JETS_ENV takes highest precedence
     settings['env'] = ENV['JETS_ENV'] if ENV['JETS_ENV']
 
     @@settings = RecursiveOpenStruct.new(settings)
