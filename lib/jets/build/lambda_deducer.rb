7cc55fe8a32a834f15438584946450ee56981144
Tung Nguyen <tongueroo@gmail.com>
2017-10-30 14:59:25 -0700

    require application_controller as part of the handler build proces

--- lib/jets/build/lambda_deducer.rb
+++ lib/jets/build/lambda_deducer.rb
@@ -12,6 +12,8 @@ class Jets::Build
 
     # Returns: [:create, :update]
     def functions
+      require "#{Jets.root}app/controllers/application_controller"
+
       # Example: require "./app/controllers/posts_controller.rb"
       require_path = @path.starts_with?('/') ? @path : "#{Jets.root}#{@path}"
       require require_path
