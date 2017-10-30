e623c28b07c1c4a1cddd399f404d5320561fc876
Tung Nguyen <tongueroo@gmail.com>
2017-10-29 20:00:36 -0700

    project jets project class

--- lib/jets/project.rb
+++ lib/jets/project.rb
@@ -1,25 +1,46 @@
-require 'ostruct'
+require 'recursive-open-struct'
+require 'yaml'
 
 # The Project default options.
 # Overriden with config/project.yml
 class Jets::Project
   class << self
+    # Allows calling top level options with the dot notation
+    # Jets::Project.project_name
+    # Jets::Project.timeout
+    # Jets::Project.level1_option.level2_option
+    # Jets::Project.level1_option.level2_option.level3_option
     def method_missing(method_name)
-      options = Jets::Project.new.options
-      options[method_name.to_sym]
+      Jets::Project.new.options.send(method_name)
     end
   end
 
-  # Defaults
+  # The options from the files get merged with the following precedence:
+  #
+  # current folder - The current folder’s config/application.yml values take the highest precedence.
+  # user - The user’s ~/.jets/application.yml values take the second highest precedence.
+  # default - The default settings bundled with the jets tool takes the lowest precedence.
+  #
+  # More info: http://rubyonjets.com/docs/settings/
+  @options = nil
   def options
-    default_project_name = "proj"  # TODO: should probably prompt user for a
-    # project name or validate a project name is configured in config/project.yml
-    OpenStruct.new(
-      project_name: default_project_name,
-      env: ENV['JETS_ENV'] || 'dev', # shouldnt really be a default here
-      timeout: 30,
-      runtime: "nodejs6.10",
-      memory_size: 1536
-    )
+    return @options if @options
+
+    project_settings = "#{Jets.root}/config/application.yml"
+    project = File.exist?(project_settings) ? YAML.load_file(project_settings) : {}
+
+    user_settings = "#{ENV['HOME']}/.jets/application.yml"
+    user = File.exist?(user_settings) ? YAML.load_file(user_settings) : {}
+
+    defaults_file = File.expand_path("../default/application.yml", __FILE__)
+    defaults = YAML.load_file(defaults_file)
+
+    options = defaults.deep_merge(user.deep_merge(project))
+    # Even though default env can be configured in the config/settings.yml file.
+    # The JETS_ENV environment variable can be set and takes higher precedence
+    # over the settings file.
+    options['env'] = ENV['JETS_ENV'] if ENV['JETS_ENV']
+
+    @options = RecursiveOpenStruct.new(options)
   end
 end
