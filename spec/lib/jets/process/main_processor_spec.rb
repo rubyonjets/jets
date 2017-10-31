7634823e307ec48e438b477e4828717a489fbb38
Tung Nguyen <tongueroo@gmail.com>
2017-10-30 17:00:31 -0700

    fix process specs

--- spec/lib/jets/process/main_processor_spec.rb
+++ spec/lib/jets/process/main_processor_spec.rb
@@ -1,19 +1,19 @@
 require "spec_helper"
 
-describe Jets::Process::ControllerProcessor do
+describe Jets::Process::MainProcessor do
   before(:all) do
     @event = { "we" => "love", "using" => "Lambda" }
     @context = {"test" => "1"}
   end
   let(:processor) do
-    Jets::Process::ControllerProcessor.new(
+    Jets::Process::MainProcessor.new(
       JSON.dump(@event),
       JSON.dump(@context),
       'handlers/controllers/posts.create' # handler
     )
   end
 
-  describe "ControllerProcessor" do
+  describe "MainProcessor" do
     it "find public_instance_methods" do
       processor.run
       expect(processor.event).to eq(@event)
