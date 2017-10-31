8ef5d5100ee256ca6f75c8b8943db94a2c406916
Tung Nguyen <tongueroo@gmail.com>
2017-10-30 21:09:15 -0700

    update sleep job

--- spec/fixtures/project/app/jobs/sleep_job.rb
+++ spec/fixtures/project/app/jobs/sleep_job.rb
@@ -1,6 +1,6 @@
 class SleepJob < ApplicationJob
   def perform
-    seconds = ENV['TEST'] ? 0 : 5
+    seconds = ENV['TEST'] ? 0 : 1
     puts("SleepJob started.  Will sleep for #{seconds} seconds.") unless ENV['TEST']
     sleep seconds
     puts("SleepJob event: #{event.inspect}") unless ENV['TEST']
