ab2964803046f31e356fddc96284802a023d57a5
Tung Nguyen <tongueroo@gmail.com>
2017-10-31 18:04:10 -0700

    fix config spec

--- spec/lib/jets/config_spec.rb
+++ spec/lib/jets/config_spec.rb
@@ -1,20 +1,19 @@
 require "spec_helper"
 
 describe Jets::Config do
-  describe "jets project config settings" do
-    it "have sane defaults" do
-      settings = Jets::Config.new.settings
-      expect(settings.timeout).to eq 10
-      expect(settings.level1.level2).to eq "test"
-    end
+  it "have good defaults" do
+    settings = Jets::Config.new.settings
+    expect(settings.timeout).to eq 10
+    expect(settings.level1.level2).to eq "test"
+  end
 
-    it "access to methods via class methods" do
-      expect(Jets::Config.runtime).to eq "nodejs6.10"
-      expect(Jets::Config.level1.level2).to eq "test"
-    end
+  it "access to methods via class methods" do
+    expect(Jets::Config.runtime).to eq "nodejs6.10"
+    expect(Jets::Config.level1.level2).to eq "test"
+  end
 
-    it "project_env alias" do
-      expect(#{Jets::Config.project_namespace}).to eq "proj-dev-1"
-    end
+  it "project_env alias" do
+    expect(Jets::Config.project_namespace).to eq "proj-dev-2"
   end
 end
+
