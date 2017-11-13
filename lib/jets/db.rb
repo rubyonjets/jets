822f8f11fa285acc2aa531b7451e97b62bdfd49d
Tung Nguyen <tongueroo@gmail.com>
2017-11-13 11:19:59 -0800

    jets db generate command, using activerecord migration generator

--- lib/jets/db.rb
+++ lib/jets/db.rb
@@ -1,8 +1,6 @@
 require "jets"
-require "bundler"
-require "rake"
-require "byebug"
-
+require "rails/generators"
+require "rails/generators/active_record/migration/migration_generator"
 
 class Jets::Db
   autoload :Tasks, 'jets/db/tasks'
@@ -12,8 +10,19 @@ class Jets::Db
   end
 
   def run_command(*args)
-    command = "bundle exec rake db:#{args.join(':')}"
-    puts "=> #{command}".colorize(:green)
-    system command
+    # generate is the only method that does not delegate to the ActiveRecord
+    # rake tasks.
+    if args[0] == "generate"
+      args.shift # remove generate
+      # Example:
+      #   args: ["generate", "create_articles", "title:string"]
+      #   args: ["create_articles", "title:string"]
+      generator = ActiveRecord::Generators::MigrationGenerator.new(args)
+      generator.create_migration_file
+    else
+      command = "bundle exec rake db:#{args.join(':')}"
+      puts "=> #{command}".colorize(:green)
+      system command
+    end
   end
 end
--- lib/jets/db/tasks.rb
+++ lib/jets/db/tasks.rb
@@ -1,13 +1,29 @@
 require "rails"
 require "active_record"
+require "recursive-open-struct"
 
 class Jets::Db::Tasks
+  # Ugly but it loads ActiveRecord database tasks
   def self.load!
     text = ERB.new(IO.read("#{Jets.root}config/database.yml")).result
     config = YAML.load(text)
     ActiveRecord::Tasks::DatabaseTasks.database_configuration = config
     ActiveRecord::Tasks::DatabaseTasks.migrations_paths = ["db/migrate"]
+
+    # Need to mock out the usage of Rails.application in:
+    # activerecord-5.1.4/lib/active_record/tasks/database_tasks.rb
+    Rails.application = RecursiveOpenStruct.new(
+      config: {
+        paths: {
+          db: ["db"],
+        }
+      },
+      paths: {
+        "db/migrate": ["db/migrate"]
+      }
+    )
     load "active_record/railties/databases.rake"
+
     load File.expand_path("../environment-task.rake", __FILE__)
   end
 end
