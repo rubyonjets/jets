76e96a44cdf5111f67f3fbb6ab50db741dd52a0a
Tung Nguyen <tongueroo@gmail.com>
2017-11-16 11:42:15 -0800

    fix add_stage_name to url

--- lib/jets/rails_overrides/url_helper.rb
+++ lib/jets/rails_overrides/url_helper.rb
@@ -28,7 +28,7 @@ private
   def add_stage_name(url)
     if request.host.include?("amazonaws.com") && url.starts_with?('/')
       stage_name = [Jets.config.short_env, Jets.config.env_extra].compact.join('_').gsub('-','_') # Stage name only allows a-zA-Z0-9_
-      url = [stage_name, url].join('')
+      url = "/#{stage_name}#{url}"
     end
 
     url
