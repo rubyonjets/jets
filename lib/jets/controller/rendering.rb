988effad9f491f8881432072b37f83af4ffd5a01
Tung Nguyen <tongueroo@gmail.com>
2017-11-17 23:14:36 -0800

    setup webpack as part of controller processing

--- lib/jets/controller/rendering.rb
+++ lib/jets/controller/rendering.rb
@@ -78,6 +78,20 @@ class Jets::Controller
       end
 
       ActionController::Base.append_view_path("app/views")
+
+      setup_webpacker
+    end
+
+    def setup_webpacker
+      require 'webpacker'
+      require 'webpacker/helper'
+      ActiveSupport.on_load :action_controller do
+        ActionController::Base.helper Webpacker::Helper
+      end
+
+      ActiveSupport.on_load :action_view do
+        include Webpacker::Helper
+      end
     end
 
     # default options:
--- lib/jets/webpacker/middleware_setup.rb
+++ lib/jets/webpacker/middleware_setup.rb
@@ -1,17 +1,6 @@
 # Breaking all the rules to get the beautiful webpacker middleware working
 
 require "webpacker"
-require "webpacker/helper"
 require "webpacker/dev_server_proxy"
-require "active_support/core_ext/object"
-require "active_support/core_ext/hash"
-
-ActiveSupport.on_load :action_controller do
-  ActionController::Base.helper Webpacker::Helper
-end
-
-ActiveSupport.on_load :action_view do
-  include Webpacker::Helper
-end
 
 Webpacker.bootstrap # whenever jets server is runs should Webpacker.bootstrap
