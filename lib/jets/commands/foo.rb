b71750a906345cef4938fe3d7c560362b39adb2b
Tung Nguyen <tongueroo@gmail.com>
2017-11-25 17:35:55 -0800

    wip: move cli to jets/commands/main

--- lib/jets/commands/foo.rb
+++ lib/jets/commands/foo.rb
@@ -1,15 +1,13 @@
-module Jets::Commands
-  class Foo < Jets::Command::Base
-    desc "bar [options]", "bar desc"
-    option :dry
-    def bar
-      puts "bar called"
-      puts "options #{options.inspect}"
-    end
+class Jets::Commands::Foo < Jets::Command::Base
+  desc "bar [options]", "bar desc"
+  option :dry
+  def bar
+    puts "bar called"
+    puts "options #{options.inspect}"
+  end
 
-    desc "baz [options]", "baz desc"
-    def baz
-      puts "baz called"
-    end
+  desc "baz [options]", "baz desc"
+  def baz
+    puts "baz called"
   end
 end
--- lib/jets/commands/main/help.rb
+++ lib/jets/commands/main/help.rb
@@ -1,63 +1,61 @@
-module Jets
-  class CLI < Command
-    class Help
-      class << self
-        def build
+module Jets::Commands::Main::Help
+  class << self
+    def build
 <<-EOL
 Builds and prepares project for AWS Lambda.  Generates a node shim and bundles a Linux Ruby in the bundled folder.  Creates a zip file to be uploaded to Lambda for each handler. This allows you to build the project and inspect the zip file that gets deployed to AWS Lambda.
 EOL
-        end
+    end
 
-        def deploy
+    def deploy
 <<-EOL
 Builds and deploys project to AWS Lambda.  This creates and or updates the CloudFormation stack.
 
 $ jets deploy
 EOL
-        end
+    end
 
-        def delete
+    def delete
 <<-EOL
 Deletes project and all its resources. You can bypass the are you sure prompt with the `--sure` flag.
 
 $ jets delete --sure
 EOL
-        end
+    end
 
-        def new_long_desc
+    def new_long_desc
 <<-EOL
 Creates a new starter jets project.  You can use the `--template` flag to use different templates.  2 supported templates: starter and barebones.  The default is a barebones starter project.
 
 $ jets new proj
 EOL
-        end
+    end
 
-        def server
+    def server
 <<-EOL
 Starts a local server for development.  The server mimics API Gateway and provides a way to test your app locally without deploying to AWS.
 
 $ jets server
 EOL
-        end
+    end
 
-        def routes
+    def routes
 <<-EOL
 Prints your apps routes.
 
 $ jets routes
 EOL
-        end
+    end
 
-        def console
+    def console
 <<-EOL
 REPL console with Jets environment loaded.
 
 $ jets console
 > Post.find("myid")
 EOL
-        end
+    end
 
-        def call
+    def call
 <<-EOL
 Invoke the lambda function on AWS. The `jets call` command can do a few extra things for your convenience:
 
@@ -116,17 +114,17 @@ Instead of calling AWS lambda remote, you can also have `jets call` use the code
 $ jets call hard_job-drive --local
 
 EOL
-        end
+    end
 
-        def generate
+    def generate
 <<-EOL
 Generate things like scaffolds. This piggy backs off of rails generators.  Example:
 
 jets generate scaffold post title:string body:text published:boolean
 EOL
-        end
+    end
 
-        def webpacker
+    def webpacker
 <<-EOL
 Webpacker commands.  Just delegates to the webpacker rake tasks. Useful command:
 
@@ -138,8 +136,6 @@ $ rake webpacker:install
 
 Run `rake -T | grep webpacker` to see all the webpacker tasks.
 EOL
-        end
-      end # class << self
-    end # class Help
-  end # class CLI < Command
+    end
+  end # class << self
 end
