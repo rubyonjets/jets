4e44bb08a82d58845c9915b3b97057ba1afad092
Tung Nguyen <tongueroo@gmail.com>
2017-11-28 13:52:47 -0800

    wip: good progress on thor group version of jets new

--- lib/jets/commands/new.rb
+++ lib/jets/commands/new.rb
@@ -2,96 +2,68 @@ require 'fileutils'
 require 'colorize'
 require 'active_support/core_ext/string'
 require 'thor'
+require 'bundler'
 
-class Jets::Commands::New < Thor::Group
-  class_option :repo, default: "tongueroo/starter", desc: "Starter repo to use."
+class Jets::Commands::Sequence < Thor::Group
+private
+  def copy
+    project_root = project_name
+    source_root = File.expand_path("../../new/templates/#{@options[:template]}", __FILE__)
+    paths = Dir.glob("#{source_root}/**/{*,.*}").
+              select {|p| File.file?(p) }
+    paths.each do |src|
+      dest = src.gsub(%r{.*starter/},'')
+      dest = "#{project_root}/#{dest}"
 
-  def start_here
-    puts "Creating new project called #{@project_name}."
-    puts "options #{options.inspect}"
-    puts "args #{args.inspect}"
-
-    # o = Thor::Options.new({}, {}, false, true)
-    # pp o.parse(args)
-    puts "start_here"
+      if File.exist?(dest) and !@options[:force]
+        puts "already exists: #{dest}".colorize(:yellow) unless @options[:quiet]
+      else
+        puts "creating: #{dest}".colorize(:green) unless @options[:quiet]
+        dirname = File.dirname(dest)
+        FileUtils.mkdir_p(dirname) unless File.exist?(dirname)
+        FileUtils.cp(src, dest)
+      end
+    end
   end
 end
 
-# class Jets::Commands::New
-#   class Generator < Thor::Group
-#     # There's got to be cleaner way of sharing these options.  Probably need to
-#     # learn how to make a thor command a group.
-#     def self.common_options
-#       [
-#         [:repo, default: "starter", desc: "Starter template to use."]
-#       ]
-#     end
+class Jets::Commands::New < Jets::Commands::Sequence
+  include Thor::Actions
 
-#     common_options.each do |params|
-#       class_option *params
-#     end
-#     # class_option :repo#, default: 'bar'
-#     def call1
-#       puts "options #{options.inspect}"
-#       puts "args #{args.inspect}"
+  def self.source_root
+    File.expand_path("new/templates/starter", File.dirname(__FILE__))
+  end
 
-#       # o = Thor::Options.new({}, {}, false, true)
-#       # pp o.parse(args)
-#       puts "call1"
-#     end
-#     # def initialize(project_name, options={})
-#     #   @project_name = project_name
-#     #   @options = options
-#     # end
+  argument :project_name
+  class_option :repo, default: "tongueroo/starter", desc: "Starter repo to use."
 
-#     # def run
-#     #   return if @options[:noop]
-#     #   copy
-#     #   # bundle_install
-#     #   # git_init
-#     #   user_message
-#     # end
+  def start_here
+    puts "Creating new project called #{project_name}."
+    puts "options #{options.inspect}"
+    puts "args #{args.inspect}"
+    puts "project_name #{project_name.inspect}"
+    directory ".", project_name
+  end
 
-#     # def copy
-#     #   project_root = @project_name
-#     #   source_root = File.expand_path("../../new/templates/#{@options[:template]}", __FILE__)
-#     #   paths = Dir.glob("#{source_root}/**/{*,.*}").
-#     #             select {|p| File.file?(p) }
-#     #   paths.each do |src|
-#     #     dest = src.gsub(%r{.*starter/},'')
-#     #     dest = "#{project_root}/#{dest}"
+  def git_init
+    git_installed = system("type git > /dev/null")
+    return unless git_installed
 
-#     #     if File.exist?(dest) and !@options[:force]
-#     #       puts "already exists: #{dest}".colorize(:yellow) unless @options[:quiet]
-#     #     else
-#     #       puts "creating: #{dest}".colorize(:green) unless @options[:quiet]
-#     #       dirname = File.dirname(dest)
-#     #       FileUtils.mkdir_p(dirname) unless File.exist?(dirname)
-#     #       FileUtils.cp(src, dest)
-#     #     end
-#     #   end
-#     # end
+    system("cd #{project_name} && git init")
+    system("cd #{project_name} && git add .")
+    system("cd #{project_name} && git commit -m 'first commit'")
+  end
 
-#     # def bundle_install
-#     #   Bundler.with_clean_env do
-#     #     system("cd #{@project_name} && BUNDLE_IGNORE_CONFIG=1 bundle install")
-#     #   end
-#     # end
+  def bundle_install
+    Bundler.with_clean_env do
+      system("cd #{project_name} && BUNDLE_IGNORE_CONFIG=1 bundle install")
+    end
+  end
 
-#     # def git_init
-#     #   git_installed = system("type git > /dev/null")
-#     #   return unless git_installed
-
-#     #   system("cd #{@project_name} && git init")
-#     #   system("cd #{@project_name} && git add .")
-#     #   system("cd #{@project_name} && git commit -m 'first commit'")
-#     # end
-
-#     # def user_message
-#     #   puts "Congrats 🎉 You have successfully created a Jets project."
-#     #   puts "To deploy the project to AWS Lambda:"
-#     #   puts "  cd #{@project_name}".colorize(:green)
-#     #   puts "  jets deploy".colorize(:green)
-#     # end
-#   end
-# end
+  def user_message
+    puts "Congrats 🎉 You have successfully created a Jets project."
+    puts "To deploy the project to AWS Lambda:"
+    puts "  cd #{project_name}".colorize(:green)
+    puts "  jets deploy".colorize(:green)
+  end
+end
