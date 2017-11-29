d39ef3cbb04a704774ae61aa20074e55ee6bad7b
Tung Nguyen <tongueroo@gmail.com>
2017-11-29 12:49:22 -0800

    WebpackerTempmlate: only force when option force provided

--- lib/jets/commands/webpacker_template.rb
+++ lib/jets/commands/webpacker_template.rb
@@ -6,12 +6,13 @@ require 'thor'
 class Jets::Commands::WebpackerTemplate < Thor::Group
   include Thor::Actions
 
+  class_option :force, desc: "Bypass confirmation and overwrite existing files."
   def self.source_root
     File.expand_path("new/templates/webpacker", File.dirname(__FILE__))
   end
 
   def reapply_templates
     # Always overwrite javascript webpacker:install created.
-    directory "app/javascript", "app/javascript", force: true
+    directory "app/javascript", "app/javascript", force: options[:force]
   end
 end
