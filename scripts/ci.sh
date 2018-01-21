d9aeb0470d6d2dfc4a7f0030e50ce412adda727a
Tung Nguyen <tongueroo@gmail.com>
2018-01-21 11:21:56 -0800

    add starter ci script

---
+++ scripts/ci.sh
@@ -0,0 +1,39 @@
+#!/bin/bash -exu
+
+sudo apt-get install -y vim
+
+cd
+APP_NAME=demo$(date +%s)
+
+# On circleci jets is set up as /usr/local/bundle/bin/jets and has BUNDLE_GEMFILE
+# set to /home/circleci/repo/Gemfile.
+# This means that the jets command is the same as the project that circleci is
+# testing.
+# Using this jets to initially create the project
+jets new $APP_NAME
+# jets new calls the following for us automatically:
+# bundle
+# jets webpacker:install
+cd $APP_NAME
+
+# Using the bundled jets now because is circleci has a bunch of BUNDLER_* env
+# variables set up and this will make it use the right bundled gems.
+bundle exec jets generate scaffold Post title:string
+# The DB_ environment variables are set up in the circleci environment variables
+# website GUI under project settings
+bundle exec jets db:create db:migrate
+
+bundle exec jets deploy
+
+APP_URL=$(bundle exec jets url)
+
+curl -v ${API_URL}/posts # should have 200 status
+
+# TODO: run capabara rack-test adapter
+# 1. download spec/features/posts_spec.rb
+# 2. bundle exec rspec
+# This tests CR of CRUD.  UPDATE and DELETE are WIP because they use PUT and DELETE
+# http methods.
+
+# cleanup the database
+bundle exec jets db:drop
