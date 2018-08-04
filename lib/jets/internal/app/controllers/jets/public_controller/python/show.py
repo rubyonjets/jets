7752ffaf14164a3a679de993e2362a7b46fa3dc9
Tung Nguyen <tongueroo@gmail.com>
2018-08-04 16:32:43 +0000

    checkpoint: public python poly controller

--- lib/jets/internal/app/controllers/jets/public_controller/python/show.py
+++ lib/jets/internal/app/controllers/jets/public_controller/python/show.py
@@ -1,7 +1,27 @@
 from pprint import pprint
 import json
-
+import os
 import os.path
+
+def handle(event, context):
+    public_path = "public%s" % event["path"]
+
+    body = None
+    if os.path.exists(public_path):
+        with open(public_path, 'r') as f:
+            body = f.read()
+    else:
+        print("path not found")
+
+    if body:
+        return response(body, 200)
+    else:
+        return response("404 Not Found: %s" % public_path, 404)
+
+
+def render(file=None):
+    with open(file,'r') as f:
+        return(f.read())
 
 def response(body, status_code=200):
     return {
@@ -13,16 +33,10 @@ def response(body, status_code=200):
             },
         }
 
-def render(file=None):
-    with open(file,'r') as f:
-        return(f.read())
-
-def handle(event, context):
-    # html = render(file="public/foo.html")
-    # return response(html, 200)
-
-    print(event)
-    return response(json.dumps(event), 200)
-
 if __name__ == '__main__':
-    print(handle({"test": 1}, {}))
+    with open('event.json') as f:
+        data = json.load(f)
+    # pprint(data)
+
+    print(handle(data, {}))
+    # print(json.dumps(handle(data, {}))) # if result is json
