238ad881e9ef26b016f45edd0e65147f98b4f029
Tung Nguyen <tongueroo@gmail.com>
2018-08-04 15:57:29 +0000

    use poly public controller

--- lib/jets/internal/app/controllers/jets/public_controller.rb
+++ lib/jets/internal/app/controllers/jets/public_controller.rb
@@ -12,14 +12,14 @@ class Jets::PublicController < Jets::Controller::Base
   python :show
 
   # TODO: Leave ruby implementation here, switch to it when ruby lambda is supported
-  def show
-    public_path = Jets.root + "public"
-    catchall_path = "#{public_path}/#{params[:catchall]}"
-    if File.exist?(catchall_path)
-      content_type = Rack::Mime.mime_type(File.extname(catchall_path))
-      render file: catchall_path, content_type: content_type
-    else
-      render file: "#{public_path}/404", status: 404
-    end
-  end
+  # def show
+  #   public_path = Jets.root + "public"
+  #   catchall_path = "#{public_path}/#{params[:catchall]}"
+  #   if File.exist?(catchall_path)
+  #     content_type = Rack::Mime.mime_type(File.extname(catchall_path))
+  #     render file: catchall_path, content_type: content_type
+  #   else
+  #     render file: "#{public_path}/404", status: 404
+  #   end
+  # end
 end
