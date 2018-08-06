1517425dc5180b65c70d0fa3d53fb312a95d06f5
Tung Nguyen <tongueroo@gmail.com>
2018-08-06 02:41:41 +0000

    add postman newman tests

--- .codebuild/integration.sh
+++ .codebuild/integration.sh
@@ -24,109 +24,31 @@ jets db:create db:migrate
 jets deploy
 
 APP_URL=$(jets url)
-# curl doesnt return error status for 500 errors so will grep for 200
-# to force a return status of false if it fails
-curl -v ${APP_URL}/posts 2>&1 | grep '< HTTP' | grep 200 # should have 200 status
-
-# TODO: run capabara rack-test adapter
-# 1. download spec/features/posts_spec.rb
-# 2. rspec
-# This tests CR of CRUD.  UPDATE and DELETE are WIP because they use PUT and DELETE
-# http methods.
-
-# cleanup the database
-jets db:drop
-
-# delete jets project
-jets delete --force
-
-exit 0
-
-# Interesting, circleci has a custom /usr/local/bundle/bin files that changes
-# how executables work. Example capture:
-# https://gist.github.com/tongueroo/1b41256d5867d14597f0cb5de67295b3
-# What results with the wrapper scripts:
-#
-#   * jets point to ~/repo/bin/jets
-#   * bundle uses /home/circleci/repo/Gemfile
-#
-# This is quite confusing when because running bundle and jets commands after ssh
-# into the box as it forces the use of /home/circleci/repo/Gemfile.
-# Additionally, when new gems are installed some of the
-# /usr/local/bundle/bin files gets overwritten, resulting in a mix bag of
-# bin wrapper files. Seems like able to reset this behavior and make my life sane by:
-#
-#   1. unset all the BUNDLER_VERSION related variables
-#   2. re-installing bundler
-#   3. use own jets wrapper script
-#
-
-function create_jets_bin() {
-  mkdir -p ~/bin
-  cat >~/bin/jets <<EOL
-#!/bin/bash
->&2 echo "Using local version at ~/repo/exe/jets"
-# unset again in case I forget in ssh shell
-unset BUNDLER_VERSION
-unset BUNDLE_PATH
-unset BUNDLE_APP_CONFIG
-unset BUNDLE_SILENCE_ROOT_WARNING
-unset BUNDLE_BIN
-exec ~/repo/exe/jets "\$@"
-EOL
-  chmod a+x ~/bin/jets
+cat > jets.postman_environment.json <<EOL
+{
+  "id": "f50e05e5-c6dd-4707-9270-82c706b2bcef",
+  "name": "Jets Test1",
+  "values": [
+    {
+      "key": "BASE_URL",
+      "value": "${APP_URL}/",
+      "description": "",
+      "type": "text",
+      "enabled": true
+    }
+  ],
+  "_postman_variable_scope": "environment",
+  "_postman_exported_at": "2018-08-06T01:39:56.523Z",
+  "_postman_exported_using": "Postman/6.2.3"
 }
+EOL
+cp .codebuild/jets.postman_collection.json .
 
-cd
-
-unset BUNDLER_VERSION
-unset BUNDLE_PATH
-unset BUNDLE_APP_CONFIG
-unset BUNDLE_SILENCE_ROOT_WARNING
-unset BUNDLE_BIN
-
-sudo apt-get install -y vim
-
-#unset GEM_HOME # dont want to unset in the script but when Im ssh in, need to unset this and re-bundle...
-
-create_jets_bin
-export PATH=~/bin:$PATH
-echo "export PATH=~/bin:$PATH" >> ~/.bashrc
-
-# On circleci, initially jets is set up as /usr/local/bundle/bin/jets and
-# has BUNDLE_GEMFILE set to /home/circleci/repo/Gemfile.
-#
-# This means that the jets command is the same as the project that circleci is
-# testing.
-# Using this jets to initially create the project
-APP_NAME=demo$(date +%s)
-cd ~/repo
-gem install bundler
-bundle
-cd
-jets new $APP_NAME # jets new runs bundle and webpacker:install
-cd $APP_NAME
-
-jets generate scaffold Post title:string
-# The DB_ environment variables are set up in the circleci environment variables
-# website GUI under project settings
-jets db:create db:migrate
-
-jets deploy
-
-APP_URL=$(jets url)
-# curl doesnt return error status for 500 errors so will grep for 200
-# to force a return status of false if it fails
-curl -v ${APP_URL}/posts 2>&1 | grep '< HTTP' | grep 200 # should have 200 status
-
-# TODO: run capabara rack-test adapter
-# 1. download spec/features/posts_spec.rb
-# 2. rspec
-# This tests CR of CRUD.  UPDATE and DELETE are WIP because they use PUT and DELETE
-# http methods.
+npm install -g newman
+newman run jets.postman_collection.json -e jets.postman_environment.json
 
 # cleanup the database
 jets db:drop
 
 # delete jets project
-jets delete --force
+# jets delete --sure
