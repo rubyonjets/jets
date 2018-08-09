1cebf658e44d2a5a781b1fe43cb2defca37f4b4e
Tung Nguyen <tongueroo@gmail.com>
2018-08-09 15:24:00 +0000

    cleanup node shim

--- lib/jets/builders/code_builder.rb
+++ lib/jets/builders/code_builder.rb
@@ -84,7 +84,7 @@ class Jets::Builders
     # In this case, we can skip a lot of the ruby related building and speed up the
     # deploy process.
     def poly_only?
-      # return true
+      return true if ENV['POLY_TRUE'] # bypass to allow rapid development of handlers
       Jets::Commands::Build.poly_only?
     end
 
