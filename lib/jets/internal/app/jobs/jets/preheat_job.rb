7bfee400d9dbee04c8372c3521d9d438082d3159
Tung Nguyen <tongueroo@gmail.com>
2018-08-13 06:46:17 +0000

    fix preheat job, use constants for scope

--- lib/jets/internal/app/jobs/jets/preheat_job.rb
+++ lib/jets/internal/app/jobs/jets/preheat_job.rb
@@ -1,20 +1,20 @@
 # Simple initial implementation of a prewarmer
 class Jets::PreheatJob < ApplicationJob
   enabled = Jets.config.prewarm.enabled
-  enabled = enabled.nil? ? true : enabled # defaults to enabled
-  concurrency = Jets.config.prewarm.concurrency || 1
-  prewarm_rate = Jets.config.prewarm.rate || '30 minutes'
-  torching = enabled && concurrency > 1
-  warming = enabled && concurrency == 1
+  ENABLED = enabled.nil? ? true : enabled # defaults to enabled
+  CONCURRENCY = Jets.config.prewarm.concurrency || 1
+  PREWARM_RATE = Jets.config.prewarm.rate || '30 minutes'
+  torching = ENABLED && CONCURRENCY > 1
+  warming = ENABLED && CONCURRENCY == 1
 
   class_timeout 30
   class_memory 1024
 
   unless Jets::Commands::Build.poly_only?
-    torching ? rate(prewarm_rate) : disable(true)
+    torching ? rate(PREWARM_RATE) : disable(true)
     def torch
       threads = []
-      concurrency.times do
+      CONCURRENCY.times do
         threads << Thread.new do
           # intentionally calling remote lambda for concurrency
           # avoid passing the _prewarm=1 flag because we want the job to do the work
@@ -27,7 +27,7 @@ class Jets::PreheatJob < ApplicationJob
       "Finished prewarming your application with a concurrency of #{concurrency}."
     end
 
-    warming ? rate(prewarm_rate) : disable(true)
+    warming ? rate(PREWARM_RATE) : disable(true)
     def warm
       Jets::Preheat.warm_all
     end
