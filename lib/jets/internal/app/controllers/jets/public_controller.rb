a14989742b8b71dba3942ded5ad2f9d10c730785
Tung Nguyen <tongueroo@gmail.com>
2018-08-13 06:58:20 +0000

    go back to python lambda functions for now

--- lib/jets/internal/app/controllers/jets/public_controller.rb
+++ lib/jets/internal/app/controllers/jets/public_controller.rb
@@ -8,20 +8,24 @@ class Jets::PublicController < Jets::Controller::Base
   layout false
   internal true
 
-  if Jets::Commands::Build.poly_only?
-    # Use python if poly only so we don't have to upload rubuy
-    python :show
-  else
-    # TODO: When ruby support is relesed, switch to it only.
-    def show
-      public_path = Jets.root + "public"
-      catchall_path = "#{public_path}/#{params[:catchall]}"
-      if File.exist?(catchall_path)
-        content_type = Rack::Mime.mime_type(File.extname(catchall_path))
-        render file: catchall_path, content_type: content_type
-      else
-        render file: "#{public_path}/404", status: 404
-      end
-    end
-  end
+  # Use python until ruby support is added.
+  # The public controller is hit a higher concurrency than other pages.
+  python :show
+
+  # if Jets::Commands::Build.poly_only?
+  #   # Use python if poly only so we don't have to upload rubuy
+  #   python :show
+  # else
+  #   # TODO: When ruby support is relesed, switch to it only.
+  #   def show
+  #     public_path = Jets.root + "public"
+  #     catchall_path = "#{public_path}/#{params[:catchall]}"
+  #     if File.exist?(catchall_path)
+  #       content_type = Rack::Mime.mime_type(File.extname(catchall_path))
+  #       render file: catchall_path, content_type: content_type
+  #     else
+  #       render file: "#{public_path}/404", status: 404
+  #     end
+  #   end
+  # end
 end
--- lib/jets/internal/app/controllers/jets/welcome_controller.rb
+++ lib/jets/internal/app/controllers/jets/welcome_controller.rb
@@ -2,18 +2,22 @@ class Jets::WelcomeController < Jets::Controller::Base
   layout false
   internal true
 
-  if Jets::Commands::Build.poly_only?
-    # Use python if poly only so we don't have to upload rubuy
-    python :index
-  else
-    # TODO: When ruby support is relesed, switch to it only.
-    def index
-      homepage = "#{Jets.root}public/index.html"
-      if File.exist?(homepage)
-        render file: homepage
-      else
-        render plain: "The public/index.html file does not exist but the root route in config/routes.rb routes to this file.  You probably want to update the root route."
-      end
-    end
-  end
+  # Use python until ruby support is added.
+  # The homepage is hit a higher concurrency than other pages.
+  python :index
+
+  # if Jets::Commands::Build.poly_only?
+  #   # Use python if poly only so we don't have to upload rubuy
+  #   python :index
+  # else
+  #   # TODO: When ruby support is relesed, switch to it only.
+  #   def index
+  #     homepage = "#{Jets.root}public/index.html"
+  #     if File.exist?(homepage)
+  #       render file: homepage
+  #     else
+  #       render plain: "The public/index.html file does not exist but the root route in config/routes.rb routes to this file.  You probably want to update the root route."
+  #     end
+  #   end
+  # end
 end
