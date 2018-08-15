b74dcff85d6fbcfa5af28f0181774632374cfe6c
Tung Nguyen <tongueroo@gmail.com>
2018-08-15 13:33:30 +0000

    fix Docker base build, comment out unneeded broken maven

--- .codebuild/scripts/install-java.sh
+++ .codebuild/scripts/install-java.sh
@@ -13,8 +13,10 @@ java -XshowSettings:properties -version
 
 ###
 # maven install: https://maven.apache.org/install.html
-wget http://mirror.cogentco.com/pub/apache/maven/maven-3/3.5.3/binaries/apache-maven-3.5.3-bin.tar.gz
-tar zxf apache-maven-3.5.3-bin.tar.gz
-mkdir -p /opt
-mv apache-maven-3.5.3 /opt/maven
-ln -s /opt/maven/bin/mvn /usr/local/bin/mvn
+#
+# maven url is down, comment out since we're not using maven
+# wget http://mirror.cogentco.com/pub/apache/maven/maven-3/3.5.3/binaries/apache-maven-3.5.3-bin.tar.gz
+# tar zxf apache-maven-3.5.3-bin.tar.gz
+# mkdir -p /opt
+# mv apache-maven-3.5.3 /opt/maven
+# ln -s /opt/maven/bin/mvn /usr/local/bin/mvn
