b069b222f8f0e9b6e75ea1728fa95af39a0e4b4d
Tung Nguyen <tongueroo@gmail.com>
2018-08-28 17:44:09 +0000

    use task name as source identifier

--- lib/jets/rule/aws_managed_rule.rb
+++ lib/jets/rule/aws_managed_rule.rb
@@ -1,10 +1,11 @@
 class Jets::Rule::AwsManagedRule < Jets::Rule::Task
   def config_rule_defaults
+    source_identifier = meth.to_s.upcase
     {
       "ConfigRuleName" => config_rule_name,
       "Source" => {
         "Owner" => "AWS",
-        "SourceIdentifier" => "INCOMING_SSH_DISABLED"
+        "SourceIdentifier" => source_identifier
       }
     }
   end
