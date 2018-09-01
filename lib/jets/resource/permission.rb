7d891b36289d56415130aa7ce5e17386843210ba
Tung Nguyen <tongueroo@gmail.com>
2018-09-01 13:13:43 +0000

    stake

--- lib/jets/resource/permission.rb
+++ lib/jets/resource/permission.rb
@@ -6,14 +6,16 @@ module Jets::Resource
       @task = task
     end
 
+    # TODO: auto-detect using the principal from the associated resource
+    # replacements for: logical_id, function_name, source_arn
     def resource
       attributes = {
-        LOGICAL_ID: {
+        SecurityJobDisableUnusedCredentialsEventsRulePermission: {
           type: "AWS::Lambda::Permission",
           properties: {
             function_name: "LAMBDA_PERMISSION_ARN",
             action: "lambda:InvokeFunction",
-            principal: "events.amazonaws.com", # todo: auto-detect using the definition
+            principal: "events.amazonaws.com",
             source_arn: "SOURCE_ARN"
           }
         }
