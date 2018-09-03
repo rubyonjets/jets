b3973f41820cd9eec5fdc710d54a3463dfa8c27a
Tung Nguyen <tongueroo@gmail.com>
2018-09-03 20:29:22 +0000

    encode binary content for the public controller

--- lib/jets/internal/app/controllers/jets/public_controller.rb
+++ lib/jets/internal/app/controllers/jets/public_controller.rb
@@ -1,4 +1,5 @@
 require "rack/mime"
+require "mimemagic"
 
 # Works for utf8 text files.
 # TODO: Add support to public_controller for binary data like images.
@@ -20,7 +21,17 @@ class Jets::PublicController < Jets::Controller::Base
       catchall_path = "#{public_path}/#{params[:catchall]}"
       if File.exist?(catchall_path)
         content_type = Rack::Mime.mime_type(File.extname(catchall_path))
-        render file: catchall_path, content_type: content_type
+        binary = !MimeMagic.by_path(catchall_path).text?
+        puts "content_type #{content_type.inspect}"
+        puts "binary #{binary}"
+
+        if binary
+          encoded_content = Base64.encode64(IO.read(catchall_path))
+          encoded_content = "encoded_content.."
+          render plain: encoded_content, content_type: content_type
+        else
+          render file: catchall_path, content_type: content_type
+        end
       else
         render file: "#{public_path}/404", status: 404
       end
