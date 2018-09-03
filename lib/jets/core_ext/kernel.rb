dc52ecdce9ff56e21735a2580f0d2f99b9bf40df
Tung Nguyen <tongueroo@gmail.com>
2018-09-03 20:11:30 +0000

    rescue encoding exception for the Jets IO flush to prevent process from crashing

--- lib/jets/core_ext/kernel.rb
+++ lib/jets/core_ext/kernel.rb
@@ -16,8 +16,18 @@ module Kernel
     @@io_buffer
   end
 
+  # Note: Writing binary data to the log will crash the process with an error like this:
+  #   jets/lib/jets/core_ext/kernel.rb:20:in `write': "\x89" from ASCII-8BIT to UTF-8 (Encoding::UndefinedConversionError)
+  # So rescuring it and discarding it to keep the process alive.
   def io_flush
-    IO.write("/tmp/jets-output.log", @@io_buffer.join("\n"))
+    chunk = @@io_buffer.join("\n")
+    begin
+      IO.write("/tmp/jets-output.log", chunk)
+    # Writing to log with binary content will crash the process so rescuing it and writing an info message.
+    rescue Encoding::UndefinedConversionError => e
+      error_message = "Encoding::UndefinedConversionError: Binary data was written to Jets::IO buffer. Writing binary data to the log will crash the process, so discarding it.  This is an info message only. If you want to return binary data please base64 encode the data."
+      IO.write("/tmp/jets-output.log", error_message)
+    end
     @@io_buffer = []
   end
 end
