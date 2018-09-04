3fe529caa0385ecdf39e8bdf380315e61eef90d0
Tung Nguyen <tongueroo@gmail.com>
2018-09-04 21:07:38 +0000

    remove unused code

--- lib/jets/cfn/template_mappers.rb
+++ lib/jets/cfn/template_mappers.rb
@@ -3,16 +3,7 @@ require 'yaml'
 
 class Jets::Cfn
   class TemplateMappers
-    # used in the parent_template.rb
-    autoload :ChildMapper, "jets/cfn/template_mappers/child_mapper"
-    autoload :ControllerMapper, "jets/cfn/template_mappers/controller_mapper"
-    autoload :FunctionMapper, "jets/cfn/template_mappers/function_mapper"
-    autoload :JobMapper, "jets/cfn/template_mappers/job_mapper"
-    autoload :RuleMapper, "jets/cfn/template_mappers/rule_mapper"
-
-    # used in the child_template.rb
     autoload :LambdaFunctionMapper, "jets/cfn/template_mappers/lambda_function_mapper"
-
     autoload :IamPolicy, "jets/cfn/template_mappers/iam_policy"
   end
 end
