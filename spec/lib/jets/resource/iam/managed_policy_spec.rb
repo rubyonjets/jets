67aced8fab4df58946d7f1b9c22833eae7b1c8ae
Tung Nguyen <tongueroo@gmail.com>
2018-09-05 00:46:46 +0000

    fix specs

--- spec/lib/jets/resource/iam/managed_policy_spec.rb
+++ spec/lib/jets/resource/iam/managed_policy_spec.rb
@@ -9,4 +9,23 @@ describe Jets::Resource::Iam::ManagedPolicy do
       expect(managed_policy.arns).to eq ["arn:aws:iam::aws:policy/AmazonEC2ReadOnlyAccess"]
     end
   end
+
+  context "multiple strings" do
+    let(:definitions) { ["AmazonEC2ReadOnlyAccess", "service-role/AWSConfigRulesExecutionRole"] }
+    it "provides the iam managed policy arn" do
+      expect(managed_policy.arns).to eq [
+        "arn:aws:iam::aws:policy/AmazonEC2ReadOnlyAccess",
+        "arn:aws:iam::aws:policy/service-role/AWSConfigRulesExecutionRole",
+      ]
+    end
+  end
+
+  context "full arn provided" do
+    let(:definitions) { ["arn:aws:iam::aws:policy/AmazonEC2ReadOnlyAccess"] }
+    it "provides the iam managed policy arn" do
+      expect(managed_policy.arns).to eq [
+        "arn:aws:iam::aws:policy/AmazonEC2ReadOnlyAccess",
+      ]
+    end
+  end
 end
