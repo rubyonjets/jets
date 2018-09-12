676b782ba8a7b0897bb7ec714f91bc9ec269d35e
Tung Nguyen <tongueroo@gmail.com>
2018-09-12 21:59:37 +0000

    fix typos

--- lib/jets/stack/parameter.rb
+++ lib/jets/stack/parameter.rb
@@ -3,18 +3,17 @@ class Jets::Stack
     autoload :Dsl, "jets/stack/parameter/dsl"
     include Base
 
-    # TODO: Move to properly class after figure out how it works
-    def build
+    def template
       definition = standarize(@definition)
     end
 
     def standarize(definition)
-      first, second, _ = defintion
-      if defintion.size == 1 && first.is_a?(Hash) # long form
+      first, second, _ = definition
+      if definition.size == 1 && first.is_a?(Hash) # long form
 
-      elsif defintion.size == 2 && second.is_a?(Hash) # medium form
+      elsif definition.size == 2 && second.is_a?(Hash) # medium form
 
-      elsif defintion.size == 2 && (second.is_a?(String) || second.is_a?(NilClass)) # short form
+      elsif definition.size == 2 && (second.is_a?(String) || second.is_a?(NilClass)) # short form
 
       else # I dont know what form
         raise "Invalid form provided. definition #{definition.inspect}"
