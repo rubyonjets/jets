90d75458e8e9954d21a38d98b3db08364c9cba25
Tung Nguyen <tongueroo@gmail.com>
2018-09-12 09:33:06 -0700

    add output to spec

--- spec/lib/jets/stack_spec.rb
+++ spec/lib/jets/stack_spec.rb
@@ -1,4 +1,5 @@
 class ExampleStack < Jets::Stack
+  ### Parameters
   # long form
   parameter(instance_type: {
     default: "t2.micro" ,
@@ -8,11 +9,35 @@ class ExampleStack < Jets::Stack
   parameter :company, default: "boltops"
   # short form
   parameter :ami_id, "ami-123"
+
+  ### Outputs
+  # long form
+  output(vpc_id: {
+    description: "vpc id",
+    value: ref("vpc"),
+  })
+  # medium form
+  output :stack_name, value: "!Ref AWS::StackName"
+  # short form
+  output :elb, value: "!Ref Elb"
+  output :elb # short form
+
+  ### Resources
+  # ...
 end
 
 describe "Stack" do
   let(:stack) { ExampleStack.new }
-  it "param" do
-    pp stack.parameters
+  it "parameters" do
+    expect(stack.parameters).to eq(
+      [[{:instance_type=>{:default=>"t2.micro", :description=>"instance type"}}],
+      [:company, {:default=>"boltops"}],
+      [:ami_id, "ami-123"]]
+    )
+  end
+
+  it "outputs" do
+    pp stack.outputs
+    # expect(stack.outputs).to ...
   end
 end
