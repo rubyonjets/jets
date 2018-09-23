984a2696ca8e6bdf12c901fdf2f6b5552597571f
Tung Nguyen <tongueroo@gmail.com>
2018-09-23 14:49:57 +0000

    use standardizer for stack resource

--- lib/jets/stack/resource.rb
+++ lib/jets/stack/resource.rb
@@ -15,29 +15,7 @@ class Jets::Stack
 
     # CloudFormation Resources reference: https://amzn.to/2NKg6ip
     def standarize(definition)
-      first, second, third, _ = definition
-      if definition.size == 1 && first.is_a?(Hash) # long form
-        first # pass through
-      elsif definition.size == 2 && second.is_a?(Hash) # medium form
-        logical_id, attributes = first, second
-        attributes.delete(:properties) if attributes[:properties].nil? || attributes[:properties].empty?
-        { logical_id => attributes }
-      elsif definition.size == 2 && second.is_a?(String) # short form
-        logical_id, type = first, second
-        { logical_id => {
-            type: type
-        }}
-      elsif definition.size == 3 && (second.is_a?(String) || second.is_a?(NilClass))# short form
-        logical_id, type, properties = first, second, third
-        template = { logical_id => {
-                       type: type
-                    }}
-        attributes = template.values.first
-        attributes[:properties] = properties unless properties.empty?
-        template
-      else # I dont know what form
-        raise "Invalid form provided. definition #{definition.inspect}"
-      end
+      Jets::Resource::Standardizer.new(definition).template
     end
 
     def replace_placeholers(template)
