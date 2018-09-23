2e601b5237aaf46b7c8dc6c971c57ce6bfb82f08
Tung Nguyen <tongueroo@gmail.com>
2018-09-23 14:24:05 +0000

    upgrade core resource to handle long, medium and short forms

--- lib/jets/resource.rb
+++ lib/jets/resource.rb
@@ -15,8 +15,36 @@ class Jets::Resource
 
   attr_reader :definition, :replacements
   def initialize(definition, replacements={})
-    @definition = definition
+    @definition = standarize(definition)
     @replacements = replacements
+  end
+
+  # CloudFormation Resources reference: https://amzn.to/2NKg6ip
+  def standarize(*definition)
+    definition = definition.flatten
+    first, second, third, _ = definition
+    if definition.size == 1 && first.is_a?(Hash) # long form
+      first # pass through
+    elsif definition.size == 2 && second.is_a?(Hash) # medium form
+      logical_id, attributes = first, second
+      attributes.delete(:properties) if attributes[:properties].nil? || attributes[:properties].empty?
+      { logical_id => attributes }
+    elsif definition.size == 2 && second.is_a?(String) # short form
+      logical_id, type = first, second
+      { logical_id => {
+          type: type
+      }}
+    elsif definition.size == 3 && (second.is_a?(String) || second.is_a?(NilClass))# short form
+      logical_id, type, properties = first, second, third
+      template = { logical_id => {
+                     type: type
+                  }}
+      attributes = template.values.first
+      attributes[:properties] = properties unless properties.empty?
+      template
+    else # I dont know what form
+      raise "Invalid form provided. definition #{definition.inspect}"
+    end
   end
 
   def logical_id
--- spec/lib/jets/resource/resource_spec.rb
+++ spec/lib/jets/resource/resource_spec.rb
@@ -1,7 +1,7 @@
 describe Jets::Resource do
   let(:resource) { Jets::Resource.new(definition, replacements) }
 
-  context "simple resource with no replacements" do
+  context "long form resource with no replacements" do
     let(:replacements) { {} }
     let(:definition) do
       {
@@ -25,7 +25,7 @@ describe Jets::Resource do
     end
   end
 
-  context "resource with replacements" do
+  context "long form resource with replacements" do
     let(:replacements) do
       { namespace: "SecurityJobCheck" }
     end
@@ -68,5 +68,87 @@ describe Jets::Resource do
       expect(event_pattern['detail']['eventSource']).to eq ["ec2.amazonaws.com"]
     end
   end
+
+  context "medium form resource with no replacements" do
+    let(:replacements) { {} }
+    let(:definition) do
+      [:rest_api,
+       type: "AWS::ApiGateway::RestApi",
+        properties: {
+          name: "demo-test"
+        }
+      ]
+    end
+
+    it "cloudformation format" do
+      # pp resource # uncomment to see and debug
+      # pp resource.attributes # uncomment to see and debug
+      # pp resource.properties # uncomment to see and debug
+      expect(resource.logical_id).to eq "RestApi"
+      expect(resource.type).to eq "AWS::ApiGateway::RestApi"
+      properties = resource.properties
+      expect(properties['Name']).to eq "demo-test"
+    end
+  end
+
+  context "medium form resource with replacements" do
+    let(:replacements) do
+      { namespace: "SecurityJobCheck" }
+    end
+    let(:definition) do
+      ["{namespace}EventsRule",
+        type: "AWS::Events::Rule",
+        properties: {
+          event_pattern: {
+            detail_type: ["AWS API Call via CloudTrail"],
+            detail: {
+              event_source: ["ec2.amazonaws.com"],
+              event_name: [
+                "AuthorizeSecurityGroupIngress",
+              ]
+            }
+          },
+          state: "ENABLED",
+          targets: [{
+            arn: "!GetAtt {namespace}LambdaFunction.Arn",
+            id: "{namespace}RuleTarget"
+          }]
+        } # closes properties
+      ]
+    end
+
+    it "cloudformation format" do
+      # pp resource  # uncomment to see and debug
+      # pp resource.logical_id # uncomment to see and debug
+      # pp resource.attributes # uncomment to see and debug
+      # pp resource.properties # uncomment to see and debug
+      expect(resource.logical_id).to eq "SecurityJobCheckEventsRule"
+      expect(resource.type).to eq "AWS::Events::Rule"
+      properties = resource.properties
+      expect(properties['State']).to eq "ENABLED"
+      # properties under EventPattern has special dasherized and pascalized casing
+      event_pattern = properties['EventPattern']
+      expect(event_pattern.key?('detail-type')).to be true
+      expect(event_pattern['detail']['eventSource']).to eq ["ec2.amazonaws.com"]
+    end
+  end
+
+  context "short form resource with no replacements" do
+    let(:replacements) { {} }
+    let(:definition) do
+      [:rest_api, "AWS::ApiGateway::RestApi", name: "demo-test" ]
+    end
+
+    it "cloudformation format" do
+      # pp resource # uncomment to see and debug
+      # pp resource.attributes # uncomment to see and debug
+      # pp resource.properties # uncomment to see and debug
+      expect(resource.logical_id).to eq "RestApi"
+      expect(resource.type).to eq "AWS::ApiGateway::RestApi"
+      properties = resource.properties
+      expect(properties['Name']).to eq "demo-test"
+    end
+  end
+
 end
 
