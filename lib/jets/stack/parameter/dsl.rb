680a77694347964133b773cb9da981175552c936
Tung Nguyen <tongueroo@gmail.com>
2018-09-25 18:57:01 +0000

    update comments

--- lib/jets/stack/parameter/dsl.rb
+++ lib/jets/stack/parameter/dsl.rb
@@ -25,13 +25,7 @@ class Jets::Stack
       end
       memoize :add_depends_on_parameters # only run once
 
-      # >> Custom.new.outputs
-      # => [#<Jets::Stack::Output:0x0000564048f68928 @subclass="Custom", @definition=[:billing_alarm]>, #<Jets::Stack::Output:0x0000564048f63f90 @subclass="Custom", @definition=[:billing_notification]>]
-      # >> Custom.new.outputs.map(&:template)
-      # => [{"BillingAlarm"=>{"Value"=>"!Ref BillingAlarm"}}, {"BillingNotification"=>{"Value"=>"!Ref BillingNotification"}}]
-      # >> Custom.new.outputs.map(&:template).map {|o| o.keys.first}
-      # => ["BillingAlarm", "BillingNotification"]
-      # >>
+      # Returns output keys associated with the stack.  They are the resource logical ids.
       def dependency_outputs(dependency)
         dependency.to_s.classify.constantize.output_keys
       end
