e210284ad3bef005a240280c2e04867004712cb5
Tung Nguyen <tongueroo@gmail.com>
2018-09-26 19:36:31 +0000

    update comment

--- lib/jets/internal/app/controllers/jets/public_controller.rb
+++ lib/jets/internal/app/controllers/jets/public_controller.rb
@@ -18,10 +18,10 @@ class Jets::PublicController < Jets::Controller::Base
         content_type = Rack::Mime.mime_type(File.extname(catchall_path))
         binary = !MimeMagic.by_path(catchall_path).text?
 
-        # TODO: binary support doesn't quite work yet.
-        # We have to add '*/*' as a binary media type to the API Gateway RestApi
-        # to enable binary support without having to send a Accept header.
-        # But doing so breaks regular form submission. Figure out how to workaround this.
+        # For binary support to work, the client also has to send the right Accept header.
+        # And the media type has been to added to api gateway.
+        # Cavaet: adding * to as the media type breaks regular form submission.
+        # All form submission gets treated as binary.
         if binary
           encoded_content = Base64.encode64(IO.read(catchall_path))
           render plain: encoded_content, content_type: content_type, base64: true
