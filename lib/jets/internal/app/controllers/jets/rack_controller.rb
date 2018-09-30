a556103e2dd4ca08324ad1b03cd021fb13ba89a1
Tung Nguyen <tongueroo@gmail.com>
2018-09-30 12:27:29 +0000

    fix rack controller resp mapping

--- lib/jets/internal/app/controllers/jets/rack_controller.rb
+++ lib/jets/internal/app/controllers/jets/rack_controller.rb
@@ -2,6 +2,9 @@ class Jets::RackController < Jets::Rack::AdapterController
   def process
     triplet = app.call(rack_env)
     resp = convert_to_api_gateway(triplet) # resp
-    render json: resp
+    # render json: resp
+    render status: resp['statusCode'],
+           headers: resp['headers'],
+           body: resp['body']
   end
 end
