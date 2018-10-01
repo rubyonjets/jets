55d312222a852dc87bb370b529b578abacba9762
Tung Nguyen <tongueroo@gmail.com>
2018-10-01 02:21:59 +0000

    start separation of ruby packager from code builder

--- lib/jets/builders.rb
+++ lib/jets/builders.rb
@@ -3,5 +3,6 @@ class Jets::Builders
   autoload :Deducer, "jets/builders/deducer"
   autoload :GemReplacer, "jets/builders/gem_replacer"
   autoload :HandlerGenerator, "jets/builders/handler_generator"
+  autoload :RubyPackager, "jets/builders/ruby_packager"
   autoload :SharedDeducer, "jets/builders/shared_deducer"
 end
---
+++ lib/jets/builders/ruby_packager.rb
@@ -0,0 +1,143 @@
+class Jets::Builders
+  class RubyPackager
+    def initialize(xxx)
+      # ???
+    end
+
+    # This is in case the user has a 2.5.x variant.
+    # Force usage of ruby version that jets supports
+    # The lambda server only has ruby 2.5.0 installed.
+    def reconfigure_ruby_version
+      ruby_version = "#{full(tmp_app_root)}/.ruby-version"
+      IO.write(ruby_version, Jets::RUBY_VERSION)
+    end
+
+    # When using submodules, bundler leaves old submodules behind. Over time this inflates
+    # the size of the the bundled gems.  So we'll clean it up.
+    def clean_old_submodules
+      # https://stackoverflow.com/questions/38800129/parsing-a-gemfile-lock-with-bundler
+      lockfile = "#{cache_area}/Gemfile.lock"
+      return unless File.exist?(lockfile)
+
+      parser = Bundler::LockfileParser.new(Bundler.read_file(lockfile))
+      specs = parser.specs
+
+      # specs = Bundler.load.specs
+      # IE: spec.source.to_s: "https://github.com/tongueroo/webpacker.git (at jets@a8c4661)"
+      submoduled_specs = specs.select do |spec|
+        spec.source.to_s =~ /@\w+\)/
+      end
+
+      # find git shas to keep
+      # IE: ["a8c4661", "abc4661"]
+      git_shas = submoduled_specs.map do |spec|
+        md = spec.source.to_s.match(/@(\w+)\)/)
+        md[1] # git_sha
+      end
+
+      # IE: /tmp/jets/demo/cache/bundled/gems/ruby/2.5.0/bundler/gems/webpacker-a8c46614c675
+      Dir.glob("#{cache_area}/bundled/gems/ruby/2.5.0/bundler/gems/*").each do |path|
+        sha = path.split('-').last[0..6] # only first 7 chars of the git sha
+        unless git_shas.include?(sha)
+          puts "Removing old submoduled gem: #{path}"
+          FileUtils.rm_rf(path) # REMOVE old submodule directory
+        end
+      end
+    end
+
+
+    # Installs gems on the current target system: both compiled and non-compiled.
+    # If user is on a macosx machine, macosx gems will be installed.
+    # If user is on a linux machine, linux gems will be installed.
+    #
+    # Copies Gemfile* to /tmp/jetss/demo/bundled folder and installs
+    # gems with bundle install from there.
+    #
+    # We take the time to copy Gemfile and bundle into a separate directory
+    # because it gets left around to act as a 'cache'.  So, when the builds the
+    # project gets built again not all the gems from get installed from the
+    # beginning.
+    def bundle_install
+      return if poly_only?
+
+      headline "Bundling: running bundle install in cache area: #{cache_area}."
+
+      copy_gemfiles
+
+      require "bundler" # dynamically require bundler so user can use any bundler
+      Bundler.with_clean_env do
+        # cd /tmp/jets/demo
+        sh(
+          "cd #{cache_area} && " \
+          "env BUNDLE_IGNORE_CONFIG=1 bundle install --path bundled/gems --without development test"
+        )
+      end
+
+      puts 'Bundle install success.'
+    end
+
+    def copy_gemfiles
+      FileUtils.mkdir_p(cache_area)
+      FileUtils.cp("#{@full_project_path}Gemfile", "#{cache_area}/Gemfile")
+      FileUtils.cp("#{@full_project_path}Gemfile.lock", "#{cache_area}/Gemfile.lock")
+    end
+
+    def setup_bundle_config
+      ensure_build_cache_bundle_config_exists!
+
+      # Override project's .bundle/config and ensure that .bundle/config matches
+      # at these 2 spots:
+      #   app_root/.bundle/config
+      #   bundled/gems/.bundle/config
+      cache_bundle_config = "#{cache_area}/.bundle/config"
+      app_bundle_config = "#{full(tmp_app_root)}/.bundle/config"
+      FileUtils.mkdir_p(File.dirname(app_bundle_config))
+      FileUtils.cp(cache_bundle_config, app_bundle_config)
+    end
+
+    # On circleci the "#{Jets.build_root}/.bundle/config" doesnt exist
+    # this only happens with ssh debugging, not when the ci.sh script gets ran.
+    # But on macosx it exists.
+    # Dont know why this is the case.
+    def ensure_build_cache_bundle_config_exists!
+      text =<<-EOL
+---
+BUNDLE_PATH: "bundled/gems"
+BUNDLE_WITHOUT: "development:test"
+EOL
+      bundle_config = "#{cache_area}/.bundle/config"
+      FileUtils.mkdir_p(File.dirname(bundle_config))
+      IO.write(bundle_config, text)
+    end
+
+    def lambdagem_options
+      {
+        s3: "lambdagems",
+        build_root: cache_area, # used in lambdagem
+        project_root: full(tmp_app_root), # used in gem_replacer and lambdagem
+      }
+    end
+
+    def extract_ruby
+      headline "Setting up a vendored copy of ruby."
+      Lambdagem.log_level = :info
+      Lambdagem::Extract::Ruby.new(Jets::RUBY_VERSION, lambdagem_options).run
+    end
+
+    def extract_gems
+      headline "Replacing compiled gems with AWS Lambda Linux compiled versions."
+      GemReplacer.new(Jets::RUBY_VERSION, lambdagem_options).run
+    end
+
+    def copy_bundled_cache
+      app_root_bundled = "#{full(tmp_app_root)}/bundled"
+      if File.exist?(app_root_bundled)
+        puts "Removing current bundled from project"
+        FileUtils.rm_rf(app_root_bundled)
+      end
+      # Leave #{Jets.build_root}/bundled behind to act as cache
+      FileUtils.cp_r("#{cache_area}/bundled", app_root_bundled)
+    end
+
+  end
+end
