2fd5f482380cb72d4463d67d456cdd5670821fe8
Tung Nguyen <tongueroo@gmail.com>
2018-10-01 12:13:25 +0000

    cleanup

--- rack_client.rb
+++
@@ -1,7 +0,0 @@
-require 'yaml'
-require 'net/http'
-require 'rack/lint'
-
-
-puts "@status #{@status}"
-puts "@response #{@response}"
--- tcp_client.rb
+++
@@ -1,9 +0,0 @@
-# tcp_client.rb
-require 'socket'
-server = TCPSocket.new 'localhost', 5678
-
-while line = server.gets
-  puts line
-end
-
-server.close
