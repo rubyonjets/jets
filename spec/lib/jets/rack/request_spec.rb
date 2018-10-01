2be6835a1d76b3f4db810f492fb28084546d9e33
Tung Nguyen <tongueroo@gmail.com>
2018-10-01 01:24:48 +0000

    update spec with stubs

--- spec/lib/jets/rack/request_spec.rb
+++ spec/lib/jets/rack/request_spec.rb
@@ -14,23 +14,23 @@ describe Jets::Rack::Request do
       it "sends request using net/http" do
         # Uncomment this stubbing to test live request
         # Will need a rack server up and running
-        # http = double(:http).as_null_object
-        # response = double(:response).as_null_object
-        # allow(response).to receive(:code).and_return("200")
-        # allow(response).to receive(:each_header).and_return({})
-        # allow(response).to receive(:body).and_return("test body")
-        # allow(http).to receive(:request) do |get, &block|
-        #   block.call(response)
-        # end
-        # allow(Net::HTTP).to receive(:start) do |host, port, &block|
-        #   block.call(http)
-        # end
+        http = double(:http).as_null_object
+        response = double(:response).as_null_object
+        allow(response).to receive(:code).and_return("200")
+        allow(response).to receive(:each_header).and_return({})
+        allow(response).to receive(:body).and_return("test body")
+        allow(http).to receive(:request) do |get, &block|
+          block.call(response)
+        end
+        allow(Net::HTTP).to receive(:start) do |host, port, &block|
+          block.call(http)
+        end
 
         resp = request.send
-        pp resp # uncomment to see and debug
+        # pp resp # uncomment to see and debug
 
-        # test values because of stubbing
-        # expect(resp).to eq({:body=>"test body", :headers=>{}, :status=>200})
+        # http value because of stubbing
+        expect(resp).to eq({:body=>http, :headers=>http, :status=>http})
       end
     end
   end
