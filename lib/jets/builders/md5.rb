8566884a0e99e12abc3ff1c0cc4e64e58bc05056
Tung Nguyen <tongueroo@gmail.com>
2018-10-20 12:27:30 +0000

    clean up md5 class

--- lib/jets/builders/md5.rb
+++ lib/jets/builders/md5.rb
@@ -9,39 +9,44 @@ require 'digest'
 # We compute the checksums before we generate the node shim handlers.
 class Jets::Builders
   class Md5
-    @@checksums = {}
-    def self.compute!
-      @@checksums = new.compute
-    end
-
-    def self.checksums
-      @@checksums
-    end
-
-    def compute
-      paths = %w[
-        stage/code
-        stage/bundled
-        stage/rack
-      ]
-      checksums = {}
-      paths.each do |path|
-        full_path = "#{Jets.build_root}/#{path}"
-        checksums[path] = dir(full_path)
+    class << self
+      @@checksums = {}
+      def checksums
+        @@checksums
       end
-      checksums
-    end
 
-    def dir(path)
-      files = Dir["#{path}/**/*"]
-      files.reject! { |f| File.directory?(f) }
-           .reject! { |f| File.symlink?(f) }
-      content = files.map do |f|
-        Digest::MD5.file(f).to_s[0..7]
-      end.join
-      md5 = Digest::MD5.new
-      md5.update(content)
-      md5.hexdigest.to_s[0..7]
+      def compute!
+        # Important to have stage/code at the end, since it will use the other
+        # 'symlinked' folders to adjust the md5 hash.
+        paths = %w[
+          stage/bundled
+          stage/rack
+          stage/code
+        ]
+        paths.each do |path|
+          @@checksums[path] = dir(path)
+        end
+        @@checksums
+      end
+
+      def dir(short_path)
+        path = "#{Jets.build_root}/#{short_path}"
+        files = Dir["#{path}/**/*"]
+        files.reject! { |f| File.directory?(f) }
+             .reject! { |f| File.symlink?(f) }
+        content = files.map do |f|
+          Digest::MD5.file(f).to_s[0..7]
+        end.join
+
+        # The stage/code md5 sha depends on the other 'symlinked' folders.
+        if short_path == "stage/code"
+          content += @@checksums.values.join
+        end
+
+        md5 = Digest::MD5.new
+        md5.update(content)
+        md5.hexdigest.to_s[0..7]
+      end
     end
   end
 end
