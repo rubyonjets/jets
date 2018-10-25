e925f098069d976b847f9c654cc94ffd29d24105
Tung Nguyen <tongueroo@gmail.com>
2018-10-25 22:03:21 +0000

    update reconfigure rails

--- lib/jets/builders/reconfigure_rails.rb
+++ lib/jets/builders/reconfigure_rails.rb
@@ -12,13 +12,48 @@ class Jets::Builders
       return unless rails?
 
       puts "Reconfiguring rails app"
-      templates = File.expand_path("./after_rack_package/rails", File.dirname(__FILE__))
+      copy_initializer
+      update_gemfile
+      update_development_environment
+    end
 
+    def copy_initializer
+      templates = File.expand_path("./after_rack_package/rails", File.dirname(__FILE__))
       relative_path = "config/initializers/jets.rb"
       src = "#{templates}/#{relative_path}"
       dest = "#{@app_root}/#{relative_path}"
       FileUtils.mkdir_p(File.dirname(dest))
       FileUtils.cp(src, dest)
+    end
+
+    def update_gemfile
+      gemfile = "#{@app_root}/Gemfile"
+      lines = IO.readlines(gemfile)
+      return if lines.detect { |l| l =~ /jets-rails/ }
+
+      # Add jets-rails gem
+      lines << "\n"
+      lines << %Q|gem "jets-rails", git: "https://github.com/tongueroo/jets-rails.git"\n|
+      write_content(gemfile, lines)
+    end
+
+    def update_development_environment
+      env_file = "#{@app_root}/config/environments/development.rb"
+      lines = IO.readlines(env_file)
+      new_lines = lines.map do |line|
+        if line =~ /(config\.file_watcher.*)/
+          "  # #{$1}"
+        else
+          line
+        end
+      end
+      write_content(env_file, new_lines)
+    end
+
+    # lines is an Array
+    def write_content(path, lines)
+      content = lines.join + "\n"
+      IO.write(path, content)
     end
 
     # Rudimentary rails detection
