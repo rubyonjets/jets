c4914dc65f331c0b3e08ec2992dc6daf05818aa4
Tung Nguyen <tongueroo@gmail.com>
2018-11-03 20:58:45 +0000

    adjust skeleton template project

--- lib/jets/commands/templates/skeleton/config/routes.rb
+++ lib/jets/commands/templates/skeleton/config/routes.rb
@@ -6,15 +6,9 @@ Jets.application.routes.draw do
   #   http://rubyonjets.com/docs/env-extra/
   root "jets/public#show"
 
-  # The jets/public#show controller serves static utf8 content out of the public folder.
-  # Replace it with your own controller to customize.
-  # Note, binary files do not get served on AWS Lambda unless you specify the Accept header.
-  # This is problematic for images requested by the Browser. IE: We don't control
-  # that accept header that the browser sends.
-  # Caveat, setting the Accept header to '*' for the entire API Gateway settings will force
-  # the public controller to serve binary data when requested by the browser, but it
-  # also results in form data always being treated as binary data also.
-  # Instead, it is recommended to serve binary data using s3.
+  # The jets/public#show controller can serve static utf8 content out of the public folder.
+  # Note, as part of the deploy process Jets uploads files in the public folder to s3
+  # and serves them out of s3 directly. S3 is well suited to serve static assets.
   # More info here: http://rubyonjets.com/docs/assets-serving/
   any "*catchall", to: "jets/public#show"
 end
