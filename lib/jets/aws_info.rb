ba0baff815cad3239b9b65dc8cef82aa359156ff
Onni Hakala <onni@keksi.io>
2018-11-05 20:43:29 +0200

    Fix notice when Jets tries to use aws-cli even when it's not available in PATH

--- lib/jets/aws_info.rb
+++ lib/jets/aws_info.rb
@@ -12,22 +12,25 @@ module Jets
 
       region = nil
 
-      # First try to get it from the ~/.aws/config
-      region = `aws configure get region 2>&1`.strip rescue nil
-      exit_code = $?.exitstatus
-      if exit_code != 0
-        exception_message = region.split("\n").grep(/botocore\.exceptions/).first
-        if exception_message
-          puts "WARN: #{exception_message}".colorize(:yellow)
-        else
-          # show full message as warning
-          puts region.colorize(:yellow)
+      # First if aws binary is available
+      # try to get it from the ~/.aws/config
+      if which('aws')
+        region = `aws configure get region 2>&1`.strip rescue nil
+        exit_code = $?.exitstatus
+        if exit_code != 0
+          exception_message = region.split("\n").grep(/botocore\.exceptions/).first
+          if exception_message
+            puts "WARN: #{exception_message}".colorize(:yellow)
+          else
+            # show full message as warning
+            puts region.colorize(:yellow)
+          end
+          puts "You can also get rid of this message by setting AWS_REGION or configuring ~/.aws/config with the region"
+          region = nil
         end
-        puts "You can also get rid of this message by setting AWS_REGION or configuring ~/.aws/config with the region"
-        region = nil
+        region = nil if region == ''
+        return region if region
       end
-      region = nil if region == ''
-      return region if region
 
       # Second try the metadata endpoint, should be available on AWS Lambda environment
       # https://stackoverflow.com/questions/4249488/find-region-from-within-an-ec2-instance
@@ -78,6 +81,24 @@ module Jets
 
     def test?
       ENV['TEST'] || ENV['CIRCLECI']
+    end
+
+    private
+
+    # Cross-platform way of finding an executable in the $PATH.
+    #
+    #   which('ruby') #=> /usr/bin/ruby
+    #
+    # source: https://stackoverflow.com/questions/2108727/which-in-ruby-checking-if-program-exists-in-path-from-ruby
+    def which(cmd)
+      exts = ENV['PATHEXT'] ? ENV['PATHEXT'].split(';') : ['']
+      ENV['PATH'].split(File::PATH_SEPARATOR).each do |path|
+        exts.each { |ext|
+          exe = File.join(path, "#{cmd}#{ext}")
+          return exe if File.executable?(exe) && !File.directory?(exe)
+        }
+      end
+      return nil
     end
   end
 end
