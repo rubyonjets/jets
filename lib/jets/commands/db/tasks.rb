bc166aeedc57631b1c1a40cf556878912f55534d
Tung Nguyen <tongueroo@gmail.com>
2018-12-22 17:04:45 +0000

    fix jets db:seed

--- lib/jets/commands/db/tasks.rb
+++ lib/jets/commands/db/tasks.rb
@@ -10,6 +10,7 @@ class Jets::Commands::Db::Tasks
     db_configs = Jets.application.config.database
     ActiveRecord::Tasks::DatabaseTasks.database_configuration = db_configs
     ActiveRecord::Tasks::DatabaseTasks.migrations_paths = ["db/migrate"]
+    ActiveRecord::Tasks::DatabaseTasks.seed_loader = Seeder.new("#{Jets.root}db/seeds.rb")
 
     # Need to mock out the usage of Rails.application in:
     # activerecord-5.1.4/lib/active_record/tasks/database_tasks.rb
@@ -26,5 +27,17 @@ class Jets::Commands::Db::Tasks
     load "active_record/railties/databases.rake"
 
     load File.expand_path("../environment-task.rake", __FILE__)
+  end
+
+  # Thanks: https://stackoverflow.com/questions/19206764/how-can-i-load-activerecord-database-tasks-on-a-ruby-project-outside-rails/24840749
+  class Seeder
+    def initialize(seed_file)
+      @seed_file = seed_file
+    end
+
+    def load_seed
+      raise "Seed file '#{@seed_file}' does not exist" unless File.file?(@seed_file)
+      load @seed_file
+    end
   end
 end
