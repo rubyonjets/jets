f984fe6604c4f211eaf69dec798615b44f228c3a
Tung Nguyen <tongueroo@gmail.com>
2019-01-13 01:10:09 +0000

    jets afterburner for rails api mode

--- lib/jets/builders/reconfigure_rails/config/initializers/jets.rb
+++ lib/jets/builders/reconfigure_rails/config/initializers/jets.rb
@@ -4,11 +4,13 @@ JetsRails.stage = ENV['JETS_STAGE'] || 'dev' # for jets-rails StageMiddleware
 s3_base_url = IO.read("#{Rails.root}/config/s3_base_url.txt").strip rescue "s3_base_url_placeholder"
 asset_host = "#{s3_base_url}/rack/public"
 Rails.application.config.action_controller.asset_host = asset_host
+Rails.application.config.public_file_server.enabled = true
 
+<% unless @api_mode -%>
 # Rails.application.config.assets.quiet = false
 Rails.application.config.assets.debug = false
 Rails.application.config.assets.compile = false
-Rails.application.config.public_file_server.enabled = true
+<% end -%>
 
 # Looks better without colorizatiion in CloudWatch logs
 Rails.application.config.colorize_logging = false
