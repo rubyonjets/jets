d484e54442eea207025667868a4c54b436b5872b
Piotr Korzuszek <piotr.korzuszek@gmail.com>
2019-03-08 13:50:44 +0100

    API Gateway: Limit resource names to 62 characters

--- spec/lib/jets/resource/api_gateway/method_spec.rb
+++ spec/lib/jets/resource/api_gateway/method_spec.rb
@@ -5,6 +5,7 @@ describe Jets::Resource::ApiGateway::Method do
     let(:route) do
       Jets::Route.new(path: "posts", method: :get, to: "posts#index")
     end
+
     it "resource" do
       expect(resource.logical_id).to eq "PostsIndexGetApiMethod"
       properties = resource.properties
@@ -16,6 +17,26 @@ describe Jets::Resource::ApiGateway::Method do
 
     it 'defaults to no authorization' do
       expect(resource.properties["AuthorizationType"]).to eq 'NONE'
+    end
+  end
+
+  context "long route" do
+    let(:route) do
+      Jets::Route.new(path: "posts/:post_id/comments/:comment_id/images/:images/source_urls/:source_urls", method: :get, to: "posts#index")
+    end
+
+    it "long route" do
+      expect(resource.logical_id).to eq "PostsPostIdCommentsCommentIdImagesImagesSourceUrlsSourceeb106e"
+      expect(resource.logical_id.size).to eq 62
+    end
+
+    it "resource" do
+      expect(resource.logical_id).to eq "PostsPostIdCommentsCommentIdImagesImagesSourceUrlsSourceeb106e"
+      properties = resource.properties
+      # pp properties # uncomment to debug
+      expect(properties["RestApiId"]).to eq "!Ref RestApi"
+      expect(properties["ResourceId"]).to eq "!Ref PostsPostIdCommentsCommentIdImagesImagesSourceUrlsSource9fe958"
+      expect(properties["HttpMethod"]).to eq "GET"
     end
   end
 
