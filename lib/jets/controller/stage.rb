3a4dc4c09af152067bfd20f6f9da6dc745380646
Tung Nguyen <tongueroo@gmail.com>
2019-05-23 23:40:12 +0000

    fix stage name in dev mode for redirect also

---
+++ lib/jets/controller/stage.rb
@@ -0,0 +1,33 @@
+class Jets::Controller
+  class Stage
+    def initialize(host, url)
+      @host, @url = host, url
+    end
+
+    def add
+      return @url unless add_stage?
+
+      stage_name = Jets::Resource::ApiGateway::Deployment.stage_name
+      "/#{stage_name}#{@url}"
+    end
+
+    def add_stage?
+      return false if on_cloud9?
+      @host.include?("amazonaws.com") && @url.starts_with?('/')
+    end
+
+    def on_cloud9?
+      self.class.on_cloud9?(@host)
+    end
+
+    class << self
+      def add(host, url)
+        new(host, url).add
+      end
+
+      def on_cloud9?(host)
+        !!(host =~ /cloud9\..*\.amazonaws\.com/)
+      end
+    end
+  end
+end
--- lib/jets/overrides/rails/common_methods.rb
+++ lib/jets/overrides/rails/common_methods.rb
@@ -2,25 +2,12 @@ module Jets::CommonMethods
   extend Memoist
   # Add API Gateway Stage Name
   def add_stage_name(url)
-    return url unless add_stage?(url)
-
-    stage_name = Jets::Resource::ApiGateway::Deployment.stage_name
-    "/#{stage_name}#{url}"
+    Jets::Controller::Stage.add(request.host, url)
   end
-
-  def add_stage?(url)
-    return false if on_cloud9?
-    request.host.include?("amazonaws.com") && url.starts_with?('/')
-  end
-  memoize :add_stage?
 
   def on_aws?
-    !request.headers['HTTP_X_AMZN_TRACE_ID'].nil? && !on_cloud9?
+    on_cloud9 = Jets::Controller::Stage.on_cloud9?(request.headers['HTTP_HOST'])
+    !request.headers['HTTP_X_AMZN_TRACE_ID'].nil? && !on_cloud9
   end
   memoize :on_aws?
-
-  def on_cloud9?
-    !!(request.headers['HTTP_HOST'] =~ /cloud9\..*\.amazonaws\.com/)
-  end
-  memoize :on_cloud9?
 end
