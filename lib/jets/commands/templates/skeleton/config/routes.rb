af4797dc2bde12c8a0e1ef91c077083d71feb887
Tung Nguyen <tongueroo@gmail.com>
2019-06-01 14:50:15 +0000

    fix link

--- lib/jets/commands/templates/skeleton/config/routes.rb
+++ lib/jets/commands/templates/skeleton/config/routes.rb
@@ -4,6 +4,6 @@ Jets.application.routes.draw do
   # The jets/public#show controller can serve static utf8 content out of the public folder.
   # Note, as part of the deploy process Jets uploads files in the public folder to s3
   # and serves them out of s3 directly. S3 is well suited to serve static assets.
-  # More info here: http://rubyonjets.com/docs/assets-serving/
+  # More info here: https://rubyonjets.com/docs/extras/assets-serving/
   any "*catchall", to: "jets/public#show"
 end
