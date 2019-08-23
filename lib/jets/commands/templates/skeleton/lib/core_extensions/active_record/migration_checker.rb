7817b245be10a905b3360ad74fc997b2e2e13add
Pei Yee Teh <peiyee.teh@grabtaxi.com>
2019-08-23 22:01:05 +0800

    Remove require rails

--- lib/jets/commands/templates/skeleton/lib/core_extensions/active_record/migration_checker.rb
+++ lib/jets/commands/templates/skeleton/lib/core_extensions/active_record/migration_checker.rb
@@ -2,15 +2,10 @@ module CoreExtensions
   module ActiveRecord
     module MigrationChecker
       def prepare_test_db
-        require 'rails'
-
         current_config = ::ActiveRecord::Base.connection_config
         all_configs = ::ActiveRecord::Base.configurations.configs_for(env_name: Jets.env)
 
         needs_update = !all_configs.all? do |db_config|
-          # Need to mock out the usage of Rails.application in:
-          # activerecord-6.0.0/lib/active_record/tasks/database_tasks.rb
-          Rails.application = Jets::Commands::Db::Tasks::Dummy::App.new
           ::ActiveRecord::Tasks::DatabaseTasks.schema_up_to_date?(db_config.config, ::ActiveRecord::Base.schema_format, nil, Jets.env, db_config.spec_name)
         end
 
