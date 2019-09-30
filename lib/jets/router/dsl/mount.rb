875e6fdecfc7f92f96e743422764a78557241c3a
Tung Nguyen <tongueroo@gmail.com>
2019-09-30 19:13:08 +0000

    allow accidental mount at / to work also

--- lib/jets/router/dsl/mount.rb
+++ lib/jets/router/dsl/mount.rb
@@ -3,6 +3,7 @@ module Jets::Router::Dsl
     # The mounted class must be a Rack compatiable class
     def mount(klass, at:)
       options = {to: "jets/mount#call", mount_class: klass}
+      at = at[1..-1] if at.starts_with?('/') # be more forgiving if / accidentally included
       at_wildcard = at.blank? ? "*path" : "#{at}/*path"
 
       any at, options
