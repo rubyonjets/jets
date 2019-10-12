968c008d1011c12d9adfc6e366c1742c505003e0
Tung Nguyen <tongueroo@gmail.com>
2019-10-12 15:27:22 +0000

    fix bucket typo

--- spec/lib/jets/cfn/template_source_spec.rb
+++ spec/lib/jets/cfn/template_source_spec.rb
@@ -3,11 +3,11 @@
 describe Jets::Cfn::TemplateSource do
   let(:path) { '/tmp/test.txt' }
   let(:template) do
-    described_class.new(path, s3_bucket: s3_backet)
+    described_class.new(path, s3_bucket: s3_bucket)
   end
 
-  context 'without s3_backet' do
-    let(:s3_backet) { nil }
+  context 'without s3_bucket' do
+    let(:s3_bucket) { nil }
     let(:body) { 'long file body' }
 
     it 'must read file from disk' do
@@ -17,9 +17,9 @@ describe Jets::Cfn::TemplateSource do
     end
   end
 
-  context 'with s3_backet' do
-    let(:s3_backet) { 'test' }
-    let(:url) { "https://s3.amazonaws.com/#{s3_backet}/test" }
+  context 'with s3_bucket' do
+    let(:s3_bucket) { 'test' }
+    let(:url) { "https://s3.amazonaws.com/#{s3_bucket}/test" }
 
     it 'must upload file to s3' do
       expect(template).to receive(:upload_file_to_s3).and_return(url)
