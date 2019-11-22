b918a9c7220512d6aeec022ab565397eafd505c0
Pradeep Kumar <pradeep@keepworks.com>
2019-11-22 18:29:56 +0530

    Fix error with 'references' in jets generate model

--- lib/jets/commands/db/tasks/dummy/app.rb
+++ lib/jets/commands/db/tasks/dummy/app.rb
@@ -4,8 +4,12 @@ module Jets::Commands::Db::Tasks::Dummy
   class App
     def config
       Config.new(
+        active_record: {
+          belongs_to_required_by_default: true
+        },
         paths: {
           db: ["db"],
+          "db/migrate": ["db/migrate"]
         }
       )
     end
