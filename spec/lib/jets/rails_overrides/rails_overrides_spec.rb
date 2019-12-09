017557fe4ed9e9968142b081990b30012b1842a9
Tung Nguyen <tongueroo@gmail.com>
2019-12-09 21:08:38 +0000

    more narrow check for /gems/ for caller_lines search
    * also fix specs and account for case
    * related #410

--- spec/lib/jets/rails_overrides/rails_overrides_spec.rb
+++ spec/lib/jets/rails_overrides/rails_overrides_spec.rb
@@ -10,9 +10,11 @@ describe "Rails Overrides" do
   end
 
   it "get_controller_name" do
-    caller_line = "/Users/tung/src/tongueroo/jets/spec/fixtures/apps/demo/app/views/posts/index.html.erb:3:in `___sers_tung_src_tongueroo_jets_spec_fixtures_apps_demo_app_views_posts_index_html_erb__1209297671030770324_70251796312860'
-    "
-    folder = view._get_containing_folder(caller_line)
+    caller_lines = [
+      "/home/ec2-user/.rbenv/versions/2.5.6/lib/ruby/gems/2.5.0/gems/haml-5.1.2/lib/haml/helpers/action_view_mods.rb:13:in `render'",
+      "/Users/tung/src/tongueroo/jets/spec/fixtures/apps/demo/app/views/posts/index.html.erb:3:in `___sers_tung_src_tongueroo_jets_spec_fixtures_apps_demo_app_views_posts_index_html_erb__1209297671030770324_70251796312860'
+    "]
+    folder = view._get_containing_folder(caller_lines)
     expect(folder).to eq "posts"
   end
 end
