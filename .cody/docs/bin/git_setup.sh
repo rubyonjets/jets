fbd53e45f7f80fe3d35e09bf756cf87330101386
Tung Nguyen <tongueroo@gmail.com>
2020-01-25 19:20:41 +0000

    fix codebuild: delete git checkout

--- .cody/docs/bin/git_setup.sh
+++ .cody/docs/bin/git_setup.sh
@@ -10,12 +10,3 @@
 git config --global user.email "tongueroo@gmail.com"
 git config --global user.name "Tung Nguyen"
 
-set +u # cb start will not have CODEBUILD_WEBHOOK_TRIGGER set
-if [ -n "$CODEBUILD_WEBHOOK_TRIGGER" ]; then # git push
-  BRANCH=$(echo $CODEBUILD_WEBHOOK_TRIGGER | sed "s/.*\///")
-elif [ -n "$CODEBUILD_SOURCE_VERSION" ]; then # cb start
-  BRANCH=$CODEBUILD_SOURCE_VERSION # contains the actual branch
-else
-  BRANCH=UNKNOWN-BRANCH
-fi
-git checkout $BRANCH
