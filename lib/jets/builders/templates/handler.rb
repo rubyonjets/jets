e8346f558cc5785f7ac093b8839f6a962636672e
Rafael Roque Aschoff <roque.rafael@gmail.com>
2022-02-03 08:51:56 -0300

    builder(templates/handler): reconnects before processing and ensures disconnect

--- lib/jets/builders/templates/handler.rb
+++ lib/jets/builders/templates/handler.rb
@@ -5,7 +5,7 @@ Jets.once  # runs once in lambda execution context
 <% @vars.functions.each do |function_name|
   handler = @vars.handler_for(function_name)
   meth = handler.split('.').last
--%>
+  -%>
 def <%= meth -%>(event:, context:)
   ActiveRecord::Base.connection.reconnect! unless ActiveRecord::Base.connection.active?
   Jets.process(event, context, "<%= handler -%>")
