ef0efb31561b0ca144e2497cb98693c5af35920a
Tung Nguyen <tongueroo@gmail.com>
2023-12-24 00:18:00 +0000

    fix s3_event and change method to s3_event_payloads

--- lib/jets/job/helpers/s3_event_helper.rb
+++ lib/jets/job/helpers/s3_event_helper.rb
@@ -1,36 +1,38 @@
 module Jets::Job::Helpers
   module S3EventHelper
-    def s3_events
+    def s3_event_payloads
       messages = event["Records"].map do |record|
         record["Sns"]["Message"]
       end
-      message.map do |message|
+      messages.map do |message|
         h = JSON.load(message)
         ActiveSupport::HashWithIndifferentAccess.new(h)
       end
     end
 
-    def s3_events?
+    def s3_event_payloads?
       event["Records"]&.any? { |r| r.dig("Sns", "Message") }
     end
 
     def s3_objects
-      records = s3_event["Records"]
-      records.map do |record|
-        record["s3"]["object"]
-      end
+      s3_event_payloads.map do |payload|
+        records = payload["Records"]
+        records.map do |record|
+          record["s3"]["object"]
+        end
+      end.flatten
     end
 
     def s3_objects?
-      s3_event["Records"]&.any? { |r| r.dig("s3", "object") }
+      s3_event_payloads["Records"]&.any? { |r| r.dig("s3", "object") }
     end
 
     # Deprecated methods below
     def s3_event
       puts "WARN: s3_event is deprecated".color(:yellow)
       puts "It can possibly drop events when come in extremely fast."
-      puts "Use s3_events instead"
-      s3_events.first
+      puts "Use s3_event_payloads instead"
+      s3_event_payloads.first
     end
 
     def s3_object
